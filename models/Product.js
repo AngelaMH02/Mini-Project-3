0ec37387120dcc561a75a9555a3290e10be11dc2
Angela Harris <harrisangela002@gmail.com>
2026-02-01 14:39:59 -0500

    Create Product model

---
+++ models/Product.js
@@ -0,0 +1,34 @@
+const { DataTypes } = require('sequelize');
+const { sequelize } = require('../config/db');
+
+/**
+ * Product Model
+ * Represents the 'Products' table in MySQL
+ */
+const Product = sequelize.define('Product', {
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: { msg: 'Title is required' },
+    },
+  },
+  description: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  price: {
+    type: DataTypes.DECIMAL(10, 2),
+    allowNull: false,
+    validate: {
+      isDecimal: { msg: 'Price must be a decimal value' },
+      min: { args: [0], msg: 'Price must be positive' },
+    },
+  },
+  productImg: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+});
+
+module.exports = { Product };
