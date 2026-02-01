33c69e74dd587d3ec73972973c8ec8ab3f0a6cc6
Angela Harris <harrisangela002@gmail.com>
2026-02-01 15:52:01 -0500

    Update receipt routes to use CRUD functions

--- routes/receiptRoutes.js
+++ routes/receiptRoutes.js
@@ -1,31 +1,26 @@
-let express = require('express');
-let router = express.Router();
-let Controllers = require('../controllers');
+const express = require('express');
+const router = express.Router();
+const {
+  getReceipts,
+  getReceiptById,
+  createReceipt,
+  updateReceipt,
+  deleteReceipt,
+} = require('../controllers/receiptController');
 
 // Get all Receipts
-router.get('/', (req, res) => {
-  Controllers.receiptController.getReceipts(req, res);
-});
+router.get('/', getReceipts);
 
 // Get Receipt by ID
-router.get('/:id', (req, res) => {
-  Controllers.receiptController.getReceiptById(req, res);
-});
+router.get('/:id', getReceiptById);
 
 // Create a new Receipt
-router.post('/', (req, res) => {
-  Controllers.receiptController.createReceipt(req, res);
-});
-
+router.post('/', createReceipt);
 // Update a Receipt by ID
-router.put('/:id', (req, res) => {
-  Controllers.receiptController.updateReceipt(req, res);
-});
+router.put('/:id', updateReceipt);
 
 // Delete a Receipt by ID
-router.delete('/:id', (req, res) => {
-  Controllers.receiptController.deleteReceipt(req, res);
-});
+router.delete('/:id', deleteReceipt);
 
 module.exports = {
   receiptRouter: router,
